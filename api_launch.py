520d7c9cb233cf0f5cb4f2dacb83ae1396c1a575
yiyiooo <yiyiooo@outlook.com>
2023-04-07 10:12:38 +0800

    update

--- api_launch.py
+++ api_launch.py
@@ -1,16 +1,39 @@
 
 from fastapi import FastAPI
+from typing import List, Optional
 import uvicorn
 from transformers import AutoModel, AutoTokenizer,AutoModelForSeq2SeqLM
 import torch
 import argparse
+from pydantic import BaseModel
 
-torch.cuda.empty_cache()
+# torch.cuda.empty_cache()
 modelName = "THUDM/chatglm-6b"
 tokenizer = None
 model = None
 
+class Item(BaseModel):  
+    msg: str  
+
+class Message(BaseModel):
+    role: str
+    content: str
+class ChatData(BaseModel):
+    messages: List[Message]
+    max_tokens: Optional[int] = 1024
+    top_p: Optional[float] = 0.9
+    temperature: Optional[float] = 0.5
+    user: Optional[str] = 'default'
+    n: Optional[int] = 1
+
+class ChatCompletion(BaseModel):
+    message: Message    
+
+class ChatResponse(BaseModel):
+    choices: List[ChatCompletion]
+
 def load_model():
+    print('load_model')
     tokenizer = AutoTokenizer.from_pretrained(modelName, trust_remote_code=True)   
     model = AutoModelForSeq2SeqLM.from_pretrained(modelName, trust_remote_code=True,device_map='auto').half() 
     model = model.eval()
@@ -20,7 +43,7 @@ MAX_BOXES = MAX_TURNS * 2
 
 def predict(input, max_length, top_p, temperature, history=None):
     if not model:
-        return '测试：这是测试内容'
+        return ('测试：这是测试内容',[])
     if history is None:
         history = []
     response, history = model.chat(tokenizer, input, history, max_length=max_length, top_p=top_p, temperature=temperature)
@@ -33,10 +56,10 @@ def convert_to_tuples(data):
     user = ''
     assistant = ''
     for item in data:
-        if item['role'] == 'user':
-            user = item['content']
-        elif item['role'] == 'assistant':
-            assistant = item['content']
+        if item.role == 'user':
+            user = item.content
+        elif item.role == 'assistant':
+            assistant = item.content
         if assistant:
             messages.append((user,assistant))
             user = ''
@@ -44,24 +67,26 @@ def convert_to_tuples(data):
     return messages
 
 @app.post('/v1/chat/completions')  
-def chat_component(data):
-    messages = data['messages']
-    max_tokens = data.get('max_tokens', 1024)
-    top_p = data.get('top_p', 0.9)
-    temperature = data.get('temperature', 0.5)
-    user = data.get('user', 'default')
-    n = data.get('n', 1)
+def chat_component(data:ChatData):
+    print(data)
+    messages = data.messages
+    max_tokens = data.max_tokens
+    top_p = data.top_p
+    temperature = data.temperature
+    user = data.user
+    n = data.n
     history = convert_to_tuples(messages)
     # 在这里执行聊天逻辑，返回聊天结果  
     speak = ''
-    if len(messages) > 0 and messages[-1]['role'] == 'user':
-        speak = messages[-1]['content']
+    if len(messages) > 0 and messages[-1].role == 'user':
+        speak = messages[-1].content
     response,_ = predict(speak, max_tokens, top_p, temperature, history)
     return {'choices': [{'message':{'role':'','content':response}}]}
 
 @app.post("/chat")
-def create_item(item):
-    msg = predict(input=item['msg'])
+def create_item(item:Item):
+    print(item)
+    msg = predict(input=item.msg)
     return msg
 
 def main(port, model_name, debug):
@@ -70,7 +95,6 @@ def main(port, model_name, debug):
     modelName = model_name
     if not debug:
         load_model()
-    print('server: http://127.0.0.1:'+str(port))
     uvicorn.run(app, host="127.0.0.1", port=port)
     print('server stop')
 
@@ -80,6 +104,6 @@ if __name__ == "__main__":
     parser.add_argument("-m", "--model_name", type=str, default="model", help="model name")
     parser.add_argument("-d", "--debug", action="store_true", help="enable debug mode")
     args = parser.parse_args()  
-
+    print(args)
     main(args.port, args.model_name, args.debug)  
 
