5ab5a2cd5c1ddd282134238d0b753e3d6952dc2a
viyiviyi <39589598+viyiviyi@users.noreply.github.com>
2023-06-29 07:18:42 +0800

    Update api_launch.py

--- api_launch.py
+++ api_launch.py
@@ -112,7 +112,7 @@ async def chat_component(data:ChatData):
         if stream:
             # 以 SSE 协议响应数据
             async def event_stream():
-                for response, _ in predict(speak, max_tokens, top_p, temperature, history, stream=True):
+                async for response, _ in predict(speak, max_tokens, top_p, temperature, history, stream=True):
                     yield {
                         "data": json.dumps({'choices': [{'delta': {'role': '', 'content': response}}]})
                     }
